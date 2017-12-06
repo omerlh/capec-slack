fa65c7d988c447cddc04954c55a78bed95ad07a2
Omer Levi Hevroni <omerl@soluto.com>
2017-12-06 22:42:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var SlackWebhook = require('slack-webhook');
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ * source: https://stackoverflow.com/a/1527820/4792970
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var slack = new SlackWebhook()
+var capecId = getRandomInt(1,508);
+console.log(`https://capec.mitre.org/data/definitions/${capecId}.html`);
+
+request(`https://capec.mitre.org/data/definitions/${capecId}.html`, function (error, response, body) {
+    if (response.statusCode >= 300) {
+        console.log(`error: ${response.statusCode}`);
+    }    
+    
+    var parsed = cheerio.load(body);
+    var description = parsed('div#Detail').children('p').first().text();
+    var headline = parsed('h2').first().text();
+
+    slack.send({
+        text: `${headline} https://capec.mitre.org/data/definitions/${capecId}.html`,
+        attachments: [
+            {
+                "text": `${description}`
+            }
+        ],
+        username: 'CAPEC',
+        icon_emoji: ':scream_cat:',
+      })
+
+});
+
